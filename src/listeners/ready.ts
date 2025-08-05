1ceedab157643a48c55eeaca59544436c37702b5
Blaadam <blaadam.gaming06@gmail.com>
2025-08-05 20:08:53 +0100

    add bug report command

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -14,7 +14,7 @@ export class ReadyListener extends Listener {
     // client.user.setActivity("under maintenance, please avoid using this service", { type: ActivityType.Custom });
     // client.user.setStatus('dnd')
 
-    // log the servers the bot is in
+    // // log the servers the bot is in
     // const guilds = client.guilds.cache.map(guild => guild.name).join(", ");
     // this.container.logger.info(
     //   `Currently in ${client.guilds.cache.size} servers: ${guilds}`
