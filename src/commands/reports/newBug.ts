fbaf0cc2c8b2e817a30fab878ba8558a375a04c0
Blaadam <blaadam.gaming06@gmail.com>
2025-08-05 20:08:53 +0100

    add bug report command

---
+++ src/commands/reports/newBug.ts
@@ -0,0 +1,55 @@
+import { Command, ApplicationCommandRegistry } from "@sapphire/framework";
+import {
+    ActionRowBuilder,
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle,
+    type ChatInputCommandInteraction,
+} from "discord.js";
+import { ApplyOptions } from "@sapphire/decorators";
+
+@ApplyOptions<Command.Options>({
+    name: "new-bug-report",
+    description: "Create a new bug report submission",
+    cooldownDelay: 10_000,
+})
+export default class ViewHistoryCommand extends Command {
+    public override registerApplicationCommands(
+        registry: ApplicationCommandRegistry
+    ) {
+        registry.registerChatInputCommand((command) => {
+            command
+                .setName(this.name)
+                .setDescription(this.description);
+        });
+    }
+
+    public async chatInputRun(interaction: ChatInputCommandInteraction) {
+        const modal = new ModalBuilder()
+            .setCustomId("bug-report-modal")
+            .setTitle("Bug Report Submission");
+
+        const bugTitle = new TextInputBuilder()
+            .setCustomId("bugTitle")
+            .setLabel("Bug Title")
+            .setPlaceholder("Enter the title of the bug")
+            .setStyle(TextInputStyle.Short);
+
+        const bugDesc = new TextInputBuilder()
+            .setCustomId("bugDesc")
+            .setLabel("Bug Description")
+            .setPlaceholder("Enter a description of the bug")
+            .setStyle(TextInputStyle.Paragraph);
+
+        const firstActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(bugTitle);
+        const secondActionRow = new ActionRowBuilder<TextInputBuilder>().addComponents(bugDesc);
+
+        modal.addComponents(
+            firstActionRow,
+            secondActionRow
+        );
+
+        return await interaction.showModal(modal);
+
+    }
+}
---
+++ src/interaction-handlers/reports/bug-report-modal.ts
@@ -0,0 +1,80 @@
+import {
+    InteractionHandler,
+    InteractionHandlerTypes,
+} from "@sapphire/framework";
+import {
+    EmbedBuilder,
+    type ModalSubmitInteraction,
+} from "discord.js";
+import "dotenv";
+require("dotenv").config();
+
+import { ApplyOptions } from "@sapphire/decorators";
+
+const receivingUser = "251442524516909058"
+
+@ApplyOptions({
+    name: "bug-report-modal",
+})
+export class ModalHandler extends InteractionHandler {
+    public constructor(
+        ctx: InteractionHandler.LoaderContext,
+        options: InteractionHandler.Options
+    ) {
+        super(ctx, {
+            ...options,
+            interactionHandlerType: InteractionHandlerTypes.ModalSubmit,
+        });
+    }
+
+    public override parse(interaction: ModalSubmitInteraction) {
+        if (interaction.customId !== this.name) return this.none();
+
+        return this.some();
+    }
+
+    public async run(interaction: ModalSubmitInteraction) {
+        const bugTitle = interaction.fields.getTextInputValue("bugTitle");
+        const bugDesc = interaction.fields.getTextInputValue("bugDesc");
+
+        if (!bugTitle || !bugDesc) {
+            return interaction.reply({
+                content: "You did not fill in the fields correctly.",
+                ephemeral: true,
+            });
+        }
+
+        // Make an embed for content data
+        const newEmbed = new EmbedBuilder()
+            .setAuthor({
+                name: interaction.user.tag,
+                iconURL:
+                    interaction.user.displayAvatarURL({ extension: "png", size: 512 }),
+            })
+            .setTitle("New Bug Report Submission")
+            .addFields(
+                { name: "Bug Title", value: bugTitle },
+                { name: "Bug Description", value: bugDesc },
+            )
+            .setTimestamp()
+            .setColor(global.embeds.embedColors.activity)
+            .setFooter(global.embeds.embedFooter);
+
+        const user = await interaction.client.users.fetch(receivingUser);
+        if (!user) {
+            return interaction.reply({
+                content: "The receiving user could not be found.",
+                ephemeral: true,
+            });
+        }
+
+        // Send the embed to the receiving user
+        const dmChannel = await user.createDM();
+        await dmChannel.send({ content: `<@${interaction.user.id}>`, embeds: [newEmbed] });
+
+        return interaction.reply({
+            content: `Your submission was received successfully and has been forwarded to support.`,
+            ephemeral: true,
+        });
+    }
+}
